b607267463cc0c96998fb5aed0e541c1112aa4cb
Sanni-Singh <aryansingh7371@gmail.com>
2024-10-22 17:20:03 +0530

    adding js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const parent = document.getElementById('box')
 const input = document.getElementById('input')
 const button = document.getElementById('button')
+// const copy = ;
 
 
 let val;
 input.addEventListener('input', (e)=>{
     val = e.target.value;
 })
-console.log(val);
 
 button.addEventListener('click',()=>{
     function generateColor(){
@@ -29,6 +29,47 @@ button.addEventListener('click',()=>{
         ele.appendChild(colorTag)
         ele.style.backgroundColor = ele.innerText;
         parent.appendChild(ele);
+
+        const  copy = document.querySelectorAll(".system");
+        copy.forEach(ele => {
+            ele.addEventListener('click' , displayColorName)
+        });
+        
         
     }
 })
+
+// parent.addEventListener('click', (e)=>{
+
+//     console.log(e);
+
+//     if(e.target.value != "" || e.target.value >=1){
+//         navigator.clipboard.writeText(e.target.value);
+//         // icon.innerHTML = "Check"
+//         // icon.title = "Passward Copied"
+
+
+//         // setTimeout(()=>{
+//         //     icon.innerHTML = "content_copy";
+//         //     icon.title = ""
+//         // },4000)
+//     }
+    
+// })
+
+function displayColorName() {
+    if(this.innerText !== "" && this.innerText.length >=1){
+        navigator.clipboard.writeText(this.innerText);
+        
+        let currentItem = this;
+        let val2 = this.innerText
+
+        this.innerText = "Copied";
+
+        setTimeout(function() {
+            currentItem.innerText = val2;
+            
+        },2000)
+    }
+}
+
